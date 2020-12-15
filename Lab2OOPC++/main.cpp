97d9e589776d2f9285109eae76a481cca6750965
AnyDodic <korri.13@inbox.ru>
2020-12-16 01:01:58 +0300

    Initial Commit

---
+++ Lab2OOPC++/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab2OOPC++
+//
+//  Created by Alexey on 16.12.2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
